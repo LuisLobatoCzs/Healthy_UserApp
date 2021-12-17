ba2e0050a13469c4a1e449fff7c23c605fc77d45
Luis Lobato <luis.lobatoczs@outlook.com>
2021-12-17 17:32:37 -0600

    Mapa a pantalla completa con ubicación predefinida

--- lib/src/pages/welcome_page.dart
+++ lib/src/pages/welcome_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mapbox_gl/mapbox_gl.dart';
 
 class WelcomePage extends StatefulWidget {
   @override
@@ -6,10 +7,30 @@ class WelcomePage extends StatefulWidget {
 }
 
 class _WelcomePageState extends State<WelcomePage> {
+  MapboxMapController? mapController;
+
+  final here = LatLng(18.991412, -98.201622);
+  final styleMap = 'mapbox://styles/luislobato/ckxax6pem0cg514qphmlky3pe';
+  final secret =
+      'sk.eyJ1IjoibHVpc2xvYmF0byIsImEiOiJja3hhczl5c3Iwc2loMzBwZng0NW1sOTZ5In0.Ff4iMP7HdlTqY0Ao7etHRQ';
+
+  _onMapCreated(MapboxMapController controller) {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('hello'),
+    return Scaffold(
+      body: map(),
+    );
+  }
+
+  MapboxMap map() {
+    return MapboxMap(
+      styleString: styleMap,
+      accessToken: secret,
+      onMapCreated: _onMapCreated,
+      initialCameraPosition: CameraPosition(target: here, zoom: 17),
     );
   }
 }
