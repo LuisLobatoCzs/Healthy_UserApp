cba77d33e4924670c448492193a9d3e0d05d56fc
Luis Lobato <luis.lobatoczs@outlook.com>
2022-03-14 17:56:52 -0600

    Bloqueo de rotación de pantalla para ejecutar la app solo en modo retrato

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:healthy/src/app.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:flutter/services.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   if (await Permission.location.request().isGranted) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     runApp(const MyApp());
   }
 }
